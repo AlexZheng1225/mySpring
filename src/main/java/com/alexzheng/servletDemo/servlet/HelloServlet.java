594185acb8042e44cf364efad6a2282672fe9de4
AlexZheng <zc_work@outlook.com>
2021-02-23 20:32:01 +0800

    IOC核心思想：创建一个servlet并运行

---
+++ src/main/java/com/alexzheng/servletDemo/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.alexzheng.servletDemo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author Alex Zheng
+ * @Date 2021/2/23 20:16
+ * @Annotation
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Spring");
+    }
+}
