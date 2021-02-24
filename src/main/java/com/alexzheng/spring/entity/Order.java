27bf88f0301fdc5bbf4f95d0aa4187bebda8fbf3
AlexZheng <zc_work@outlook.com>
2021-02-24 08:20:35 +0800

    手写IOC容器：注销测试代码

--- src/main/java/com/alexzheng/spring/entity/Order.java
+++ src/main/java/com/alexzheng/spring/entity/Order.java
@@ -1,22 +1,22 @@
-package com.alexzheng.spring.entity;
-
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-/**
- * @Author Alex Zheng
- * @Date 2021/2/3 21:31
- * @Annotation
- */
-@Data
-@Component("order")
-public class Order {
-
-    @Value("zc11225")
-    private String orderId;
-
-    @Value("1000.0")
-    private Float proce;
-
-}
+//package com.alexzheng.spring.entity;
+//
+//import lombok.Data;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @Author Alex Zheng
+// * @Date 2021/2/3 21:31
+// * @Annotation
+// */
+//@Data
+//@Component("order")
+//public class Order {
+//
+//    @Value("zc11225")
+//    private String orderId;
+//
+//    @Value("1000.0")
+//    private Float proce;
+//
+//}
--- src/main/java/com/alexzheng/spring/test/Test.java
+++ src/main/java/com/alexzheng/spring/test/Test.java
@@ -1,29 +1,29 @@
-package com.alexzheng.spring.test;
-
-import javafx.application.Application;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-/**
- * @Author Alex Zheng
- * @Date 2021/2/3 21:23
- * @Annotation
- */
-public class Test {
-    public static void main(String[] args) {
-        //加载IOC容器-ApplicationContext 除了ApplicationContext还有BeanFactory
-        //使用注解不能使用 new ClassPathXmlApplicationContext();
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.alexzheng.spring.entity");
-        //获取类信息
-        System.out.println(applicationContext.getBean("account"));//要写小写类名
-        //获取IOC中所有的Bean个数
-        System.out.println(applicationContext.getBeanDefinitionCount());
-        //返回String类型的数组,获取所有bean的名字
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames(); //获取所有的bean的名字
-        for (String beanDefinitionName:beanDefinitionNames){
-            System.out.println(beanDefinitionName);
-            System.out.println(applicationContext.getBean(beanDefinitionName));
-        }
-    }
-}
+//package com.alexzheng.spring.test;
+//
+//import javafx.application.Application;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+///**
+// * @Author Alex Zheng
+// * @Date 2021/2/3 21:23
+// * @Annotation
+// */
+//public class Test {
+//    public static void main(String[] args) {
+//        //加载IOC容器-ApplicationContext 除了ApplicationContext还有BeanFactory
+//        //使用注解不能使用 new ClassPathXmlApplicationContext();
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.alexzheng.spring.entity");
+//        //获取类信息
+//        System.out.println(applicationContext.getBean("account"));//要写小写类名
+//        //获取IOC中所有的Bean个数
+//        System.out.println(applicationContext.getBeanDefinitionCount());
+//        //返回String类型的数组,获取所有bean的名字
+//        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames(); //获取所有的bean的名字
+//        for (String beanDefinitionName:beanDefinitionNames){
+//            System.out.println(beanDefinitionName);
+//            System.out.println(applicationContext.getBean(beanDefinitionName));
+//        }
+//    }
+//}
